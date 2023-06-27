c3dc1f4166bb74fda637b30d404f98fb975b81ce
jpedrosantosb <jpedrosantosb@gmail.com>
2023-06-26 21:40:20 -0300

    API de cep

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,6 +1,7 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms'
+import { ConsultaCepService } from '../service/consulta-cep.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,13 +10,20 @@ import { NgForm } from '@angular/forms'
 })
 export class CadastroComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private consultaCepService: ConsultaCepService) { }
 
   ngOnInit(): void {
   }
 
+  consultaCep(ev: any) {
+    const cep = ev.target.value;
+    return this.consultaCepService.getConsultaCep(cep).subscribe(resultado => {
+      console.log(resultado);
+    })
+  }
+
   cadastrar(form: NgForm) {
-    if(form.valid) {
+    if (form.valid) {
       this.router.navigate(['./sucesso'])
     } else {
       alert('Formulário Inválido!')
